f6de9b38f8d2c23e8ef68d4701ae810cd4bd29e9
mozzart <Brunocorona@live.com>
2023-07-04 00:20:06 -0300

    Feat: Carrousel added

--- src/my-work/my-work.js
+++ src/my-work/my-work.js
@@ -2,11 +2,46 @@ import React from "react";
 import StarsBackground from "./../animations/starsBackground/starBackground";
 import * as S from "./my-work.styled";
 import AnimationText from "./../animations/text/animationText";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function MyWork() {
+    const settings = {
+        arrow:true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+      };
   return (
     <S.backgroundMyWork>
       <StarsBackground />
+      <S.carouselPosition>
+      <S.carouselPos>
+      <Slider className="carrossel-container" {...settings}>
+      <div>
+        <h3>Slide 1</h3>
+      </div>
+      <div>
+        <h3>Slide 2</h3>
+      </div>
+      <div>
+        <h3>Slide 3</h3>
+      </div>
+      <div>
+        <h3>Slide 4</h3>
+      </div>
+      <div>
+        <h3>Slide 5</h3>
+      </div>
+      <div>
+        <h3>Slide 6</h3>
+      </div>
+    </Slider>
+    </S.carouselPos>
+    </S.carouselPosition>
     </S.backgroundMyWork>
   );
 }
--- src/my-work/my-work.styled.js
+++ src/my-work/my-work.styled.js
@@ -13,10 +13,57 @@ export const backgroundMyWork = styled.div`
   top: 0;
   left: 0;
 `
-export const starPosition = styled.div`
+
+export const carouselPosition = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+export const carouselPos = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 80%;
+
+  .slide {
+    
+  }
+
+  .slick-slide img {
+    width: 100%;
+    height: auto;
+  }
+
+  .slick-dots{
     position: absolute;
-    width: 50%;
-    Star{
-        
-    }
-`;
+    bottom: 1rem;
+  }
+  .slick-dots li button:before {
+  font-size: 12px;
+  }
+  
+
+  .carrossel-container {
+    
+  width: 90%;
+  height: 80%;
+  background-color: rgba(224, 234, 235, 0.2);
+  border-radius: 5%;
+  overflow: hidden;
+  box-shadow: 0 5px 0 rgb(0, 0, 0, 0.2);
+  animation: fade-in 0.8s 2.1s forwards cubic-bezier(0.11, 0, 0.5, 0);
+  h3{
+    color: white;
+    width: 90%;
+    height: auto;
+    background-color: #000;
+    border-radius: 5%;
+  }
+  }
+
+
+`
